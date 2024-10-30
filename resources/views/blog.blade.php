21e155110e994fda28583dbf76e9f20970785d3a
hikmalryvaldi <hikmalryvaldi@gmail.com>
2024-10-30 13:37:51 +0700

    Video 3 - Struktur Folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Blog</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/blog">Blog</a>
+  <a href="/contact">Contact</a>
+
+  <h1>Halaman Blog</h1>
+
+  <h3>Article 1</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eligendi sit, accusantium facilis, aperiam voluptatibus accusamus quam quidem dignissimos reiciendis doloremque est incidunt numquam labore soluta laboriosam earum odio sequi!</p>
+
+  <h3>Article 2</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eligendi sit, accusantium facilis, aperiam voluptatibus accusamus quam quidem dignissimos reiciendis doloremque est incidunt numquam labore soluta laboriosam earum odio sequi!</p>
+  
+</body>
+</html>
